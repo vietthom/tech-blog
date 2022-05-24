ccaa5d55f721a416eef1c5a06007acbd405b6bda
thomle0418 <91095934+thomle0418@users.noreply.github.com>
2022-05-24 09:08:12 -0600

    backend completed.

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const apiRoutes = Require('./apiRoutes');
 const postRoutes = require('./apiRoutes/postRoutes');
 const commentRoutes = require('./apiRoutes/commentRoutes');
+const dashboardRoutes = require('../routes/dashboardRoutes');
 const { loginView, signupView } = require('../controllers/userController');
 const { homePosts, createPostView } = require('../controllers/postController');
 
@@ -11,7 +12,7 @@ router.get('/signup', signupView);
 router.get('/newPost', createPostView);
 
 router.use('/posts', postRoutes );
-
+router.use('/dashboard', dashboardRoutes);
 router.use('/comments', commentRoutes);
 router.use('/api', apiRoutes);
 
