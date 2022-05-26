7172f87586085001647d6fbf8164e1004c81dabb
thomle0418 <91095934+thomle0418@users.noreply.github.com>
2022-05-25 21:43:14 -0600

    creating function to make text area resize to the content

---
+++ public/js/app.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    // function to make textareas resize to the content
+    $("textarea").each(function () {
+        this.setAttribute("style", "height:" + (this.scrollHeight) + "px;overflow-y:hidden;");
+      }).on("input", function () {
+        this.style.height = "auto";
+        this.style.height = (this.scrollHeight) + "px";
+      });
+});
